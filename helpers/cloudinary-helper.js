a31e98368faf1aeef4ecce33542fefc190089b34
TANMAYBORHADE <tanmayborhade2005@gmail.com>
2025-05-15 13:59:03 +0530

    made changes

--- helpers/cloudinary-helper.js
+++ helpers/cloudinary-helper.js
@@ -1,16 +1,21 @@
+
+
 const cloudinary = require('../config/cloudinary')
 
-const uploadToCloudinary = async(filePath)=>{
-    try{
-        const result = await cloudinary.uploader.upload(filePath)
+const uploadToCloudinary = async (filePath) => {
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto" 
+        })
         return {
             url: result.secure_url,
-            publicId: result.public_id
+            publicId: result.public_id,
+            resourceType: result.resource_type
         }
-    }catch(error){
-        console.error('Error while uploading to cloudinary',error)
+    } catch (error) {
+        console.error('Error while uploading to cloudinary', error)
         throw new Error('Error while uploading to cloudinary')
     }
 }
 
-module.exports = uploadToCloudinary; 
+module.exports = uploadToCloudinary;
